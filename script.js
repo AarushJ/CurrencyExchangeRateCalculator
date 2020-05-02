d6d34113d3824623af31c2cc9315e916abdfcc38
Aarush Juneja <avijuneja.cs@gmail.com>
2020-05-02 16:35:33 +0530

    Added js for fetching exchange rate and updating dom
    1. Added js for fetching exchange rate per USD of all currency codes.
    
    2. Found out perUSD price of currencies selected in both the dropdowns and from that calculated the exchange rate of the two currencies.
    
    3. Added swap button functionality
    
    4. Added event listeners for dropdowns and input fields.

---
+++ script.js
@@ -0,0 +1,47 @@
+const currencyOneElem = document.getElementById('currency-one');
+const currencyTwoElem = document.getElementById('currency-two');
+const amountOne = document.getElementById('amount-one');
+const amountTwo = document.getElementById('amount-two');
+
+const rateElement = document.getElementById('rate');
+const swapButton = document.getElementById('swap');
+
+calculate();
+
+// fetch exchange rates and update the DOM
+function calculate(){
+  const currencyOneCode = currencyOneElem.value;
+  const currencyTwoCode = currencyTwoElem.value;
+
+  fetchPerUSDRate().then(data => {
+    const currencyOnePerUSD = data.rates[currencyOneCode];
+    const currencyTwoPerUSD = data.rates[currencyTwoCode];
+
+    // 1$ = currencyOnePerUSD currencyOneCode
+    // 1$ = currencyTwoPerUSD currencyTwoCode
+    // currencyTwoPerUSD currencyTwoCode = currencyOnePerUSD currencyOneCode
+    // 1 currencyOneCode = (currencyTwoPerUSD/currencyOnePerUSD) currencyTwoCode
+    const rate = (currencyTwoPerUSD/currencyOnePerUSD).toFixed(3);
+    rateElement.innerText = `1 ${currencyOneCode} = ${rate} ${currencyTwoCode}`;
+    amountTwo.value = (amountOne.value*rate).toFixed(3);
+  });
+}
+
+async function fetchPerUSDRate(){
+  // this api endpoints gives rate of all currencies wrt USD
+  const response = await fetch(`https://api.exchangerate-api.com/v6/latest`);
+  return response.json();
+}
+
+// Event listeners start here
+currencyOneElem.addEventListener('change', calculate);
+currencyTwoElem.addEventListener('change', calculate);
+amountOne.addEventListener('input', calculate);
+amountTwo.addEventListener('input', calculate);
+
+swapButton.addEventListener('click', () => {
+  const temp = currencyOneElem.value;
+  currencyOneElem.value = currencyTwoElem.value;
+  currencyTwoElem.value = temp;
+  calculate();
+});
